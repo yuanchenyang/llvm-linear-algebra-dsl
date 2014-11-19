cf83af8e38e275be408cff0b4be64c91afca1759
Chenyang Yuan <yuanchenyang@gmail.com>
2014-11-18 21:18:13 -0800

    Added stuff

--- lms/lmstest.scala
+++ lms/lmstest.scala
@@ -11,13 +11,50 @@ trait Ackermann extends Dsl {
   }
 }
 
+trait Mul extends Dsl {
+  def m(a: Int): Rep[Int => Int] = fun { (b: Rep[Int]) =>
+    a * b
+  }
+}
+
+class Matrix[T] {
+  var x: Int
+  var y: Int
+  var data: Array[T]
+
+  def this(x: Int, y: Int) = {
+    this()
+    this.x = x
+    this.y = y
+    this.data = NewArray[T](x * y)
+  }
+
+  def getIndex(x: Int, y: Int): Int {
+    this.x * y + this.y
+  }
+
+  def getItem(x: Int, y: Int): T {
+    this.data[this.getIndex(x, y)]
+  }
+  def setItem(x: Int, y: Int, item: T) {
+    this.data[this.getIndex(x, y)] = item
+  }
+}
+
+
+
 object Main {
-  def specialize(m: Int): DslDriver[Int,Int] = new DslDriver[Int,Int] with Ackermann {
-    def snippet(n: Rep[Int]): Rep[Int] = a(m)(n)
+  // def specialize(m: Int): DslDriver[Int,Int] = new DslDriver[Int,Int] with Ackermann {
+  //   def snippet(n: Rep[Int]): Rep[Int] = a(m)(n)
+  // }
+
+  def specialize(a: Int): DslDriver[Int,Int] = new DslDriver[Int,Int] with Mul {
+    def snippet(b: Rep[Int]): Rep[Int] = m(a)(b)
   }
 
   def main(args: Array[String]) {
     // Specialize Ackermann function to concrete value 2
-    println(specialize(2).code)
+    var m = new Matrix(2, 3)
+    //println(specialize(0).code)
   }
 }
--- sandbox.rkt
+++ sandbox.rkt
@@ -1,6 +1,42 @@
 #lang racket
 
+(define (symbolic? e)
+  (or (symbol? e)
+      (list?   e)))
+
 (define (power x n)
   (if (= n 0)
       '1
       `(* ,x ,(power x (- n 1)))))
+
+(define-syntax-rule (if. pred t f)
+  (cond [(symbolic? pred)
+         (list 'if pred t f)]
+        [pred t]
+        [else f]))
+
+(define (=. e1 e2)
+  (if (or (symbolic? e1)
+          (symbolic? e2))
+      (list '= e1 e2)
+      (= e1 e2)))
+
+(define (*. e1 e2)
+  (if (or (symbolic? e1)
+          (symbolic? e2))
+      (list '* e1 e2)
+      (* e1 e2)))
+
+(define (-. e1 e2)
+  (if (or (symbolic? e1)
+          (symbolic? e2))
+      (list '- e1 e2)
+      (- e1 e2)))
+
+(define (power. x n)
+  (if. (=. n 0)
+       1
+       (*. x (power. x (-. n 1)))))
+
+(power. 'x 4)
+(power. 4 'x)
