349754c96466ded535edde7f437f67096f93de6e
Leonard Truong <leonardtruong@gmail.com>
2014-12-10 13:33:00 -0800

    Ast->LLVM add

--- backend.rkt
+++ backend.rkt
@@ -1,4 +1,5 @@
 #lang racket
+
 (require "nodes.rkt")
 (require racket/pretty)
 (require racket-llvm/unsafe)
@@ -9,22 +10,51 @@
 (define i (symbol "i"))
 (define j (symbol "j"))
 
-(define tree
-  (func-decl
-   (symbol "matrix-add")
-   (list a b c)
-   (list
-    (for i (num 0) (num 10) (num 1)
-	 (for j (num 0) (num 10) (num 1)
-	      (assign (array-ref c (add (mul i (num 10)) j))
-		      (add (array-ref a (add (mul i (num 10)) j))
-			   (array-ref b (add (mul i (num 10)) j)))))))))
+(define add-func
+  (return (add (symbol "x") (symbol "y"))))
+
+(define (compile-ast-to-llvm node builder env)
+  (cond [(return? node) (LLVMBuildRet builder (compile-ast-to-llvm
+					       (return-target node) builder env))]
+	[(add? node) (let ([op1 (compile-ast-to-llvm (add-op1 node) builder env)]
+			   [op2 (compile-ast-to-llvm (add-op2 node) builder env)])
+		       (LLVMBuildAdd builder op1 op2 "a"))]
+	[(symbol? node) (hash-ref env (symbol-name node))]
+	[else (error "Unsupport node")]))
 
 (define (do-math program)
   (begin
     (define context (LLVMContextCreate))
     (define module (LLVMModuleCreateWithNameInContext "jit-module" context))
     (define int-type (LLVMInt32TypeInContext context))
-    (pretty-print program)))
+    (define fun-type (LLVMFunctionType int-type (list int-type int-type) false))
+    (define add-fun (LLVMAddFunction module "add" fun-type))
+    (let ()
+      (define x (LLVMGetParam add-fun 0))
+      (define y (LLVMGetParam add-fun 1))
+      (LLVMSetValueName x "x")
+      (LLVMSetValueName y "y")
+      (define env
+	(make-hash
+	 (list (cons "x" x)
+               (cons "y" x))))
+      (define entry (LLVMAppendBasicBlockInContext context add-fun "entry"))
+      (define builder (LLVMCreateBuilderInContext context))
 
-(do-math tree)
+      (LLVMPositionBuilderAtEnd builder entry)
+      (compile-ast-to-llvm program builder env)
+      (LLVMDumpModule module)
+    )))
+
+(do-math add-func)
+
+;; (define tree
+;;   (func-decl
+;;    (symbol "matrix-add")
+;;    (list a b c)
+;;    (list
+;;     (for i (num 0) (num 10) (num 1)
+;; 	 (for j (num 0) (num 10) (num 1)
+;; 	      (assign (array-ref c (add (mul i (num 10)) j))
+;; 		      (add (array-ref a (add (mul i (num 10)) j))
+;; 			   (array-ref b (add (mul i (num 10)) j)))))))))
--- nodes.rkt
+++ nodes.rkt
@@ -1,18 +1,15 @@
 #lang racket
 
-(provide for
-	 assign
-	 add
-	 mul
-	 array-ref
-	 num
-	 symbol)
+(provide (all-defined-out))
 
 (struct for (loop-var init end incr body) #:transparent)
 (struct assign (target value) #:transparent)
 (struct add (op1 op2) #:transparent)
 (struct mul (op1 op2) #:transparent)
+(struct sub (op1 op2) #:transparent)
+(struct div (op1 op2) #:transparent)
 (struct array-ref (arr index) #:transparent)
 (struct num (value) #:transparent)
 (struct symbol (name) #:transparent)
 (struct func-decl (name args body) #:transparent)
+(struct return (target) #:transparent)
