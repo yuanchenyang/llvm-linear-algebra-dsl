d4937502581bdc7d85df6998d01cce7a0fab0aae
Chenyang Yuan <yuanchenyang@gmail.com>
2014-12-09 17:04:01 -0800

    Added partial support for partial evaluation

--- sandbox.rkt
+++ sandbox.rkt
@@ -27,16 +27,35 @@
       (list '* e1 e2)
       (* e1 e2)))
 
+(define (/. e1 e2)
+  (if (or (symbolic? e1)
+          (symbolic? e2))
+      (list '/ e1 e2)
+      (/ e1 e2)))
+
 (define (-. e1 e2)
   (if (or (symbolic? e1)
           (symbolic? e2))
       (list '- e1 e2)
       (- e1 e2)))
 
+(define (even?. e)
+  (if (symbolic? e)
+      (list 'even? e)
+      (even? e)))
+
+
+(define (square. e)
+  (if (symbolic? e)
+      (list 'square e)
+      (*. e e)))
+
 (define (power. x n)
   (if. (=. n 0)
        1
-       (*. x (power. x (-. n 1)))))
+       (if. (even?. n)
+            (square. (power. x (/. n 2)))
+            (*. x (power. x (-. n 1))))))
 
-(power. 'x 4)
-(power. 4 'x)
+(power. 'x 41)
+(power. 2 41)
