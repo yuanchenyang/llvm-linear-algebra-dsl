d6d2140c65941fa8a62fe55a9525a0164c473801
Leonard Truong <leonardtruong@gmail.com>
2014-12-10 16:40:43 -0800

    Seperate compile logic

--- backend.rkt
+++ backend.rkt
@@ -5,14 +5,24 @@
 (require racket-llvm/unsafe)
 (require "utils.rkt")
 
+(define (compile-for-node node builder env context)
+  '())
 
-(define (compile-ast-to-llvm node builder env)
-  (cond [(return? node) (LLVMBuildRet builder (compile-ast-to-llvm
-					       (return-target node) builder env))]
-	[(add? node) (let ([op1 (compile-ast-to-llvm (add-op1 node) builder env)]
-			   [op2 (compile-ast-to-llvm (add-op2 node) builder env)])
-		       (LLVMBuildAdd builder op1 op2 (gen-unique-symbol)))]
-	[(symbol? node) (hash-ref env (symbol-name node))]
+(define (compile-return node builder env context)
+  (LLVMBuildRet
+   builder
+   (compile-ast-to-llvm (return-target node) builder env context)))
+
+(define (compile-add node builder env context)
+  (let ([op1 (compile-ast-to-llvm (add-op1 node) builder env context)]
+	[op2 (compile-ast-to-llvm (add-op2 node) builder env context)])
+    (LLVMBuildAdd builder op1 op2 (gen-unique-symbol))))
+
+(define (compile-ast-to-llvm node builder env context)
+  (cond [(return? node)   (compile-return   node builder env context)]
+	[(add? node)      (compile-add      node builder env context)]
+	[(for-node? node) (compile-for-node node builder env context)]
+	[(symbol? node)   (hash-ref env (symbol-name node))]
 	[else (error "Unsupport node")]))
 
 (define (process-params func params index)
@@ -40,7 +50,7 @@
 
       (LLVMPositionBuilderAtEnd builder entry)
 
-      (compile-ast-to-llvm (func-decl-body program) builder env)
+      (compile-ast-to-llvm (func-decl-body program) builder env context)
       (LLVMDumpModule module)
 
       (let-values (((err) (LLVMVerifyModule module 'LLVMReturnStatusAction)))
@@ -61,6 +71,46 @@
 (define add-func
   (func-decl "add" (list (symbol "x") (symbol "y"))
 	     (return (add (symbol "x") (symbol "y")))))
+
+(define a (symbol "a"))
+(define b (symbol "b"))
+(define c (symbol "c"))
+(define i (symbol "i"))
+(define j (symbol "j"))
+
+(define array-add
+  (func-decl
+   "matrix-add"
+   (list a b c)
+   (list
+    (for-node i (num 0) (num 10) (num 1)
+	 (for-node j (num 0) (num 10) (num 1)
+	      (assign (array-reference c (add (mul i (num 10)) j))
+		      (add (array-reference a (add (mul i (num 10)) j))
+			   (array-reference b (add (mul i (num 10)) j)))))))))
+
+(require ffi/unsafe
+	 racket/flonum)
+
+(define A
+  (let ([v (make-flvector 100)])
+    (begin0 v
+      (for ([x (in-range 100)])
+	(flvector-set! v x 1.0)))))
+
+(define B
+  (let ([v (make-flvector 100)])
+    (begin0 v
+      (for ([x (in-range 100)])
+	(flvector-set! v x 1.0)))))
+
+(define C
+  (let ([v (make-flvector 100)])
+    (begin0 v
+      (for ([x (in-range 100)])
+	(flvector-set! v x 0.0)))))
+
+(LLVMCreateGenericValueOfPointer (flvector->cpointer A))
 
 (test-begin
    "Test simple add"
