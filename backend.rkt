d3ec1db82ed9ebaa35935a39d1e597514fa30cf3
Leonard Truong <leonardtruong@gmail.com>
2014-12-10 19:46:16 -0800

    More work on compiling for nodes

--- backend.rkt
+++ backend.rkt
@@ -5,8 +5,35 @@
 (require racket-llvm/unsafe)
 (require "utils.rkt")
 
+
+(define (builder->function builder)
+  (LLVMGetBasicBlockParent (LLVMGetInsertBlock builder)))
+
+
+(define (compile-assign node builder env context)
+  (let ([value (compile-ast-to-llvm (assign-value node) builder env context)]
+	[target (compile-ast-to-llvm (assign-target node) builder env context)])
+    (LLVMBuildStore builder value target)))
+
 (define (compile-for-node node builder env context)
-  '())
+  (begin
+    ;; Create a new block
+    (define block (LLVMAppendBasicBlockInContext
+		   context (builder->function builder) "loop"))
+    ;; Hardcoding int-type
+    (define int-type (LLVMInt32TypeInContext context))
+    ;; Allocate loop variable
+    (define loop-var-name (symbol-name (for-node-loop-var node)))
+    (define loop-var (LLVMBuildAlloca builder int-type loop-var-name))
+    (define old-val (hash-ref env loop-var '()))
+    (hash-set! env loop-var-name loop-var)
+    (compile-ast-to-llvm (assign (for-node-loop-var node) (for-node-init node))
+    			 builder env context)
+    (LLVMBuildBr builder block)
+    (LLVMPositionBuilderAtEnd builder block)
+    (compile-ast-to-llvm (for-node-body node) builder env context)
+    (hash-remove! env loop-var-name)
+    ))
 
 (define (compile-return node builder env context)
   (LLVMBuildRet
@@ -18,12 +45,23 @@
 	[op2 (compile-ast-to-llvm (add-op2 node) builder env context)])
     (LLVMBuildAdd builder op1 op2 (gen-unique-symbol))))
 
+(define (compile-num node context)
+  (begin
+    (define int-type (LLVMInt32TypeInContext context))
+    (LLVMConstInt int-type (num-value node) #t)))
+
+(define (compile-array-ref node builder env context)
+  (error "compile-array-ref not implemented"))
+
 (define (compile-ast-to-llvm node builder env context)
-  (cond [(return? node)   (compile-return   node builder env context)]
-	[(add? node)      (compile-add      node builder env context)]
-	[(for-node? node) (compile-for-node node builder env context)]
-	[(symbol? node)   (hash-ref env (symbol-name node))]
-	[else (error "Unsupport node")]))
+  (cond [(return? node)          (compile-return    node builder env context)]
+	[(add? node)             (compile-add       node builder env context)]
+	[(for-node? node)        (compile-for-node  node builder env context)]
+	[(assign? node)          (compile-assign    node builder env context)]
+	[(num? node)             (compile-num       node context)]
+	[(array-reference? node) (compile-array-ref node builder env context)]
+	[(symbol? node)          (hash-ref env (symbol-name node))]
+	[else (error "Unsupported node")]))
 
 (define (process-params func params index)
   (if (null? params) '()
@@ -56,12 +94,12 @@
       (let-values (((err) (LLVMVerifyModule module 'LLVMReturnStatusAction)))
        (when err
          (display err) (exit 1)))
-      (define arg1 (LLVMCreateGenericValueOfInt int-type (car args) #t))
-      (define arg2 (LLVMCreateGenericValueOfInt int-type (car (cdr args)) #t))
+      (define int-args (map (lambda (arg)
+			      (LLVMCreateGenericValueOfInt int-type arg)) args))
       (LLVMLinkInJIT)
       (define ee (LLVMCreateExecutionEngineForModule module))
       
-      (define output (LLVMRunFunction ee fun (list arg1 arg2)))
+      (define output (LLVMRunFunction ee fun int-args))
       (LLVMGenericValueToInt output #t)
     )))
 
@@ -82,12 +120,11 @@
   (func-decl
    "matrix-add"
    (list a b c)
-   (list
-    (for-node i (num 0) (num 10) (num 1)
-	 (for-node j (num 0) (num 10) (num 1)
-	      (assign (array-reference c (add (mul i (num 10)) j))
-		      (add (array-reference a (add (mul i (num 10)) j))
-			   (array-reference b (add (mul i (num 10)) j)))))))))
+   (for-node i (num 0) (num 10) (num 1)
+	     (for-node j (num 0) (num 10) (num 1)
+		       (assign (array-reference c (add (mul i (num 10)) j))
+			       (add (array-reference a (add (mul i (num 10)) j))
+				    (array-reference b (add (mul i (num 10)) j))))))))
 
 (require ffi/unsafe
 	 racket/flonum)
@@ -111,6 +148,8 @@
 	(flvector-set! v x 0.0)))))
 
 (LLVMCreateGenericValueOfPointer (flvector->cpointer A))
+
+(do-math array-add (list A B C))
 
 (test-begin
    "Test simple add"
