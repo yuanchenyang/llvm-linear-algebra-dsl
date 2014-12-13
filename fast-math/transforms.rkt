41647fef71a6331482440ff685b3dd7f1b1b9a43
Robert Huang <robert.huang@berkeley.edu>
2014-12-12 20:58:59 -0800

    fix mixed indenting for transforms

--- fast-math/transforms.rkt
+++ fast-math/transforms.rkt
@@ -34,10 +34,10 @@
 (define (replace-symbol-for-node tree replacer)
   (match-let ([(for-node loopvar start end incr body) tree])
     (for-node (replacer loopvar)
-	      (replacer start)
-	      (replacer end)
-	      (replacer incr)
-	      (map replacer body))))
+              (replacer start)
+              (replacer end)
+              (replacer incr)
+              (map replacer body))))
 
 (define (replace-symbol-assign tree replacer)
   (match-let ([(assign target value) tree])
@@ -55,31 +55,31 @@
 (define (replace-symbol to-replace new)
   (define (replacer tree)
     (cond [(for-node? tree) (replace-symbol-for-node tree replacer)]
-	  [(symbol? tree) (if (equal? (symbol-name tree) to-replace)
-			      (symbol new) tree)]
-	  [(assign? tree) (replace-symbol-assign tree replacer)]
-	  [(array-reference? tree) (replace-symbol-array-reference tree replacer)]
-	  [(add? tree) (replace-symbol-binop tree add replacer)]
-	  [(mul? tree) (replace-symbol-binop tree mul replacer)]
-	  [(num? tree) tree]
-	  [else (error tree)]))
+          [(symbol? tree) (if (equal? (symbol-name tree) to-replace)
+                            (symbol new) tree)]
+          [(assign? tree) (replace-symbol-assign tree replacer)]
+          [(array-reference? tree) (replace-symbol-array-reference tree replacer)]
+          [(add? tree) (replace-symbol-binop tree add replacer)]
+          [(mul? tree) (replace-symbol-binop tree mul replacer)]
+          [(num? tree) tree]
+          [else (error tree)]))
   replacer)
 
 (define (fuse loop1 loop2)
   (match-let* ([(for-node loopvar start end incr body) loop1]
-	       [(for-node loopvar2 start2 end2 incr2 body2) loop2]
-	       [body-replaced (map (replace-symbol (symbol-name loopvar) (symbol-name loopvar2)) body)])
+               [(for-node loopvar2 start2 end2 incr2 body2) loop2]
+               [body-replaced (map (replace-symbol (symbol-name loopvar) (symbol-name loopvar2)) body)])
     (struct-copy for-node loop2 [body (foldr fuse-loops '() (append body-replaced body2))])))
 
 (define (fuse-loops prev result)
   (cond [(null? result) (list prev)]
-	[(and (for-node? prev) (for-node? (car result)))
-	 (cons (fuse prev (car result)) (cdr result))]
-	[else (cons prev result)]))
+        [(and (for-node? prev) (for-node? (car result)))
+         (cons (fuse prev (car result)) (cdr result))]
+        [else (cons prev result)]))
 
 (define (fusion-pass tree)
   (cond [(func-decl? tree) (struct-copy func-decl tree [body (fusion-pass (func-decl-body tree))])]
-	[(list? tree) (foldr fuse-loops '() tree)]
-	[else (error "Unsupported node type")]))
+        [(list? tree) (foldr fuse-loops '() tree)]
+        [else (error "Unsupported node type")]))
 
 (pretty-print (fusion-pass tree))
