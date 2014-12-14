00388e646d53e2d4613c729a66d0e55f9cefd508
Leonard Truong <leonardtruong@gmail.com>
2014-12-14 12:16:19 -0800

    Move frontend tests to seperate file.

--- fast-math/frontend.rkt
+++ fast-math/frontend.rkt
@@ -131,21 +131,3 @@ Special
          ;;compiled))]))
          (pretty-print tree)
          (compiled arg ...)))]))
-
-(define-optimized (test-add mat (a mat) (b mat))
-  (+. a (+. b a)))
-
-(define-optimized (test-convolve mat (a mat) (b mat))
-  (convolve. a b))
-
-(let ([a (make-constant-matrix "a" '((1 2 3) (4 5 6)))]
-      [b (make-constant-matrix "b" '((7 8 9) (10 11 12)))]
-      [c (make-constant-matrix "a" '((1 1 1 1 1)
-                                     (1 5 5 5 1)
-                                     (1 5 5 5 1)
-                                     (1 5 5 5 1)
-                                     (1 1 1 1 1)))]
-      [d (make-constant-matrix "b" '((0 1 2) (3 4 5) (6 7 8)))])
-  (matrix-display (test-add a b))
-  (matrix-display (test-convolve c d))
-  )
